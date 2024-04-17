f2f9ec5ce50467ee8909b5015b92bd53a2be7c1d
elvinhray <elvinhray@gmail.com>
2024-04-17 12:24:34 -0600

    ahora si

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import path, { resolve } from 'node:path';
 import { ViteMinifyPlugin} from 'vite-plugin-minify'
 import htmlPurge from 'vite-plugin-purgecss';
 import handlebars from 'vite-plugin-handlebars';
+import hbsContext  from './context/variables';
 
-import handlerBarsContext from './variables.js';
 
 export default defineConfig({
     base: "/proyectoweb/",
